c19a8d13e9b61210af715e05b978c57055d9637b
Sil van Diepen <me@sil.mt>
2024-02-15 18:15:27 +0100

    add nav

---
+++ src/components/Navigation.vue
@@ -0,0 +1,29 @@
+<template>
+    <nav :class="bemm()">
+      <Button type="ghost" :icon="Icons.ARROW_LEFT" @click="goHome()" />
+    </nav>
+</template>
+<script setup lang="ts">
+import { useBemm } from "bemm";
+import Button from "./Button.vue";
+import { Icons } from "open-icon";
+import {useRouter} from 'vue-router';
+import {RouteName} from '@/router';
+
+const router = useRouter();
+const bemm = useBemm('nav');
+
+const goHome = ()=>{
+
+    router.push({name: RouteName.HOME});
+    console.log('hoiii')
+}
+</script>
+
+<style lang="scss">
+
+.nav{
+    position: fixed;
+    padding: 4vw 8vw;
+}
+</style>
